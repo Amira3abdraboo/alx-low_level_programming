6dfba8190b85ec909757a5aab41026ba5ae048c2
Amira3abdraboo <Amira3abdraboo@gmail.com>
2023-04-20 13:37:49 -0700

    A

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
  * main : is a program that prints put function
- * Return : 0 (succesful)
+ * Return : 0 (success)
  */	
 int main(void)
 {
-puts(" \""Programming is like building a multilingual puzzle");
-return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
 
