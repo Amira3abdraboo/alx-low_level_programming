0fa925d5436879c3ef303dc08e6816e00b02506d
Amira3abdraboo <Amira3abdraboo@gmail.com>
2023-04-27 12:08:13 -0700

    E

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,6 @@
 #include"main.h"
 /**
- *print_most_numbers - prints the numbers, from 0 to 9,
- *followed by a new line
- *do not print 2 and 4
+ *print_most_numbers - prints the numbers, except  print 2 and 4
  *Return: void
  */
 
