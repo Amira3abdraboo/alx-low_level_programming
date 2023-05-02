962263f196119e4ed98ee3b230704b6a7a214a0e
Amira3abdraboo <Amira3abdraboo@gmail.com>
2023-05-02 14:16:10 -0700

    E

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ *puts_half - prints half of a string, followed by a new line
+ *@str: input
+ *Return: half of input
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+		;
+	i++
+	for (i /= 2; i != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
