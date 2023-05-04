70cac8eac9111060a90750454f6de0e398eba271
Amira3abdraboo <Amira3abdraboo@gmail.com>
2023-05-04 12:27:26 -0700

    E

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ **cap_string - Write a function that capitalizes all words of a string
+ *@s: input string
+ *Return: string with captialized words
+ */
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimit = 1;
+
+	while (*s)
+	{
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (islower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit = 0;
+		}
+		else
+			foundDelimit = 0;
+	s++
+	}
+	return (ptr);
+}
