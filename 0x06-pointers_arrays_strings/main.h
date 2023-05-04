6dbaf8fac6b7b065eab1511dfce0fde40bebc81d
Amira3abdraboo <Amira3abdraboo@gmail.com>
2023-05-04 04:27:01 -0700

    E

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,7 +9,7 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
-char *string_toupper(char *n);
+char *string_toupper(char *a);
 char *cap_string(char *);
 char *cap_string(char *);
 
