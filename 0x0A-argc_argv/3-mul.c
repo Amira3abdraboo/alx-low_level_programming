9fc53b1dd8e0a41fabd75d51c71c69574f5fad3f
Amira3abdraboo <Amira3abdraboo@gmail.com>
2023-05-12 13:36:31 -0700

    P

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- *main - multiplies two numbers.
+ *main - multiplies two numbers
  *@argc: number of arguments
  *@argv: array of arguments
  *Return: 0 on Success, 1 on fail
@@ -9,9 +9,9 @@ int main(int argc, char *argv[])
 {
 	int sum;
 
-	if (argc == 3)
+	if (sum == 3)
 	{
-		sum = atoi(argv[1]) *  atoi(argv[2]);
+		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
 	}
 	else
