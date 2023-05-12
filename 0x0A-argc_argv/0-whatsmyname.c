fa225057e569163912143ff033d21f6fb92b26e5
Amira3abdraboo <Amira3abdraboo@gmail.com>
2023-05-12 12:07:09 -0700

    R

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include"main.h"
 /**
  *main- prints the programs name folowed by a new line
- *@argc: int
- *@argv: an array
+ *@argc: number of arguments
+ *@argv: array of arguments
  *Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
