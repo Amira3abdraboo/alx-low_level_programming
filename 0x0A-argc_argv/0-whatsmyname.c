1aa625ad90876c8b4aaf1ef8aa46fe8e638c0cbd
Amira3abdraboo <Amira3abdraboo@gmail.com>
2023-05-12 11:59:33 -0700

    R

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ *main- prints the programs name folowed by a new line
+ *@argc: int
+ *@argv: an array
+ *Return: 0
+ */
+int main(int argc, char *argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
